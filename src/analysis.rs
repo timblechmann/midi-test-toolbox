d1b53c720aad0dd499e41524ab4f0bf2e3061d8a
Tim Blechmann <tim@klingt.org>
2024-10-24 12:42:00 +0800

    Analysis: improve implementation
    * use generics
    * use map/reduce instead of fold

--- src/analysis.rs
+++ src/analysis.rs
@@ -1,12 +1,13 @@
+use std::cmp::Ord;
 use std::time::Duration;
 
-pub fn median(vec: &[Duration]) -> Duration {
+pub fn median<T: Ord + Copy>(vec: &[T]) -> T {
     let mut sorted = vec.to_vec();
     sorted.sort();
     sorted[sorted.len() / 2]
 }
 
 pub fn mean(vec: &[Duration]) -> Duration {
-    let accum = vec.iter().fold(Duration::from_millis(0), |acc, e| acc + *e);
-    accum / (vec.len() as u32)
+    let accum = vec.iter().map(|e| *e).reduce(|acc, e| acc + e);
+    accum.unwrap() / (vec.len() as u32)
 }
